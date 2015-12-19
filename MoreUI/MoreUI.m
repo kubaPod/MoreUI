6d2ea27bb34f3d3439d5bdbb5df3194f87f2935c
kuba <kuba@gluonvision.com>
2015-12-19 20:12:01 +0100

    readMe and template updated

--- MoreUI/MoreUI.m
+++ MoreUI/MoreUI.m
@@ -1,11 +1,15 @@
 
 BeginPackage["MoreUI`"]
 
-	
+	ActionNestedMenu
 
 Begin["`Private`"]
 
-	
+	actionNestedMenu[menuLabel_ -> spec_] := DynamicModule[{menu},
+  parseSpec[menu, spec, 1];
+  
+  Button[menuLabel, Appearance -> "DefaultButton"]
+  ]
 
 End[];	
 	
