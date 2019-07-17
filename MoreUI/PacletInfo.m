52eccbf705e1bc82066bad15c634afe8148f1781
Kuba <kuba.pod@gmail.com>
2019-07-17 09:43:54 +0200

    0.3.0

--- MoreUI/PacletInfo.m
+++ MoreUI/PacletInfo.m
@@ -1,5 +1,5 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-Paclet[Name -> "MoreUI", Version -> "0.2.0", MathematicaVersion -> "10.4+", 
+Paclet[Name -> "MoreUI", Version -> "0.3.0", MathematicaVersion -> "10.4+", 
  Description -> "Gui utilities for Mathematica", 
  Creator -> "Kuba (kuba.pod@gmail.com)", 
  Extensions -> {{"Kernel", Context -> "MoreUI`"}, {"FrontEnd"}}]
